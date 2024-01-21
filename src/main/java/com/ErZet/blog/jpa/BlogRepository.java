d611b5c0792b54b7aafbeef1c65fd94d7676d992
ErZet99 <patryk231123@gmail.com>
2024-01-21 16:51:20 +0100

    Add repositories interfaces

---
+++ src/main/java/com/ErZet/blog/jpa/BlogRepository.java
@@ -0,0 +1,16 @@
+package com.ErZet.blog.jpa;
+
+import com.ErZet.blog.model.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+@Repository
+public interface BlogRepository extends MongoRepository<Blog, String> {
+    Blog deleteByBlogId(String blogId);
+    List<Blog> findByUserId(String blogId, Pageable pageable);
+    Blog findByBlogId(String blogId, Pageable pageable);
+}

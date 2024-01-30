64c0d87f7cfdee72df350e2de2b0a5a25001911e
ErZet99 <patryk231123@gmail.com>
2024-01-30 23:12:01 +0100

    Integrate an in-memory(first-level) cache

--- src/main/java/com/ErZet/blog/service/BlogService.java
+++ src/main/java/com/ErZet/blog/service/BlogService.java
@@ -5,9 +5,14 @@ import com.ErZet.blog.dto.CreateBlogRequest;
 import com.ErZet.blog.dto.UpdateBlogRequest;
 import com.ErZet.blog.jpa.BlogRepository;
 import com.ErZet.blog.model.Blog;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -17,6 +22,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@Slf4j
+@CacheConfig(cacheNames="blogs")
 public class BlogService {
     @Autowired
     private BlogRepository blogRepository;
@@ -29,6 +36,7 @@ public class BlogService {
         return blogRepository.save(blog);
     }
 
+    @CachePut(key="#updatedBlogRequest.blogId") // update cache also
     public Blog updateBlog(UpdateBlogRequest updatedBlogRequest) throws Exception{
         Blog blog = blogRepository.findByBlogId(updatedBlogRequest.getBlogId());
         if(ObjectUtils.isEmpty(blog)) return null;      // handling if blog is not found
@@ -37,10 +45,12 @@ public class BlogService {
         return blogRepository.save(blog);
     }
 
+    @CacheEvict(key="#blogId")   // delete cache also
     public Blog deleteBlog(String blogId) throws Exception{
         return blogRepository.deleteByBlogId(blogId);
     }
 
+    @Cacheable(key="#blogId")    // before ask database for blog with blogId, ask cache (hashTable) for that
     public Blog getBlog(String blogId) throws Exception{
         return blogRepository.findByBlogId(blogId);
     }

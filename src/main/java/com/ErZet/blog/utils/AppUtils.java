f685d54f94a4e26e54f6c7fff3ce25822c0caf41
ErZet99 <patryk231123@gmail.com>
2024-02-01 18:16:50 +0100

    Utils for random number generator (4 digits).

--- src/main/java/com/ErZet/blog/utils/AppUtils.java
+++ src/main/java/com/ErZet/blog/utils/AppUtils.java
@@ -1,4 +1,13 @@
 package com.ErZet.blog.utils;
 
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
 public class AppUtils {
+    public int getFourDigitNumber() {
+        Random random = new Random();
+        return random.nextInt(9000) + 1000;
+    }
 }
